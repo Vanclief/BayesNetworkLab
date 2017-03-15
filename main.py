cc875d0e6a1739f913d8fe69e3455b64ab58b372
Franco Valencia <franco.avalencia@gmail.com>
2017-03-14 22:44:03 -0600

    More code clean up

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ Node = __import__('node')
 # Serialize Input into lines
 
 
-def process_input(lines):
-    for line in lines[:]:
-        if line.startswith("[Nodes]"):
-            lines.pop(0)
-            return lines
-        lines.pop(0)
+def process_input():
+    lines = []
+
+    for line in fileinput.input():
+        lines.append(line)
+
+    lines = list(map(lambda x: x.strip('').rstrip(
+        '\n').rstrip('\r').replace(' ', ''), lines))
+
+    return lines
+
+# Parse the input lines into a dictionary of lists containing Nodes,
+# Probabilities and Queries
 
 
 def parse_input(lines):
@@ -29,51 +36,10 @@ def parse_input(lines):
             sections[current_section].append(line)
     return sections
 
-# def input_to_nodes(lines):
-    # nodes = {}
-    # for node in lines[0].split(', '):
-    # nodes[node] = ()
-    # for line in lines[:]:
-    # if line.startswith("[Probabilities]"):
-    # lines.pop(0)
-    # return lines, nodes
-    # lines.pop(0)
-
-# def input_to_probs(lines, nodes):
-    # for line in lines[:]:
-    # if line.startswith("\n"):
-    # lines.pop(0)
-    # lines.pop(0)
-    # return lines, nodes
-    # statement = line.split(' = ')
-    # posterior = statement[0].split('|')
-    # if len(posterior) > 1:
-    # posterior[1] = posterior[1].split(', ')
-    # probability = statement[1][:-1]
-    # print(posterior)
-    # print(nodes)
-    # print(probability)
-
-    # print(nodes[statement[0][1:]])
-    # for s in statement:
-    # 	print(s)
-    # print("\n")
-
-    # lines.pop(0)
-
-# def input_to_queries():
 
 def main():
 
-    lines = []
-    nodes = {}
-
-    for line in fileinput.input():
-        lines.append(line)
-
-    lines = list(map(lambda x: x.strip('').rstrip(
-        '\n').rstrip('\r').replace(' ', ''), lines))
-
+    lines = process_input()
     sections = parse_input(lines)
     print(sections)
 

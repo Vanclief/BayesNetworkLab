861be5413c277f95fd990901e45765a6e2d70387
SquGus <squgus@gmail.com>
2017-03-14 19:49:41 -0600

    Parses each probability statement

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
 import fileinput
+import re
 
-# class Node:
-# 	# Node class
-# 	parents = []
-# 	probability_table = {}
+class Node:
+	parents = []
+	probability_table = {}
 
-# 	def add_parent():
+	def add_parent(parent):
+		parents.append(parent)
 
-
-# 	def add_prob(nodes, probability):
-# 		nodes.
+	def add_probability(given_probs):
+		for prob in given_probs:
+			print(prob)
 
 def process_input(lines):
 	for line in lines[:]:
 		if line.startswith("[Nodes]"):
-			lines.remove(line)
+			lines.pop(0)
 			return lines
-		lines.remove(line)
+		lines.pop(0)
 
 def input_to_nodes(lines):
 	nodes = {}
 	for node in lines[0].split(', '):
-		nodes[node] = "Node here"
-	lines.remove(lines[0])
-	lines.remove(lines[1])
-	return lines, nodes
+		nodes[node] = Node()
+	for line in lines[:]:
+		if line.startswith("[Probabilities]"):
+			lines.pop(0)
+			return lines, nodes
+		lines.pop(0)
 
+def input_to_probs(lines, nodes):
+	for line in lines[:]:
+		if line.startswith("\n"):
+			lines.pop(0)
+			lines.pop(0)
+			return lines, nodes
+		statement = line.split(' = ')
+		posterior = statement[0].split('|')
+		if len(posterior) > 1:
+			posterior[1] = posterior[1].split(', ')
+		probability = statement[1][:-1]
+		print(posterior)
+		print(probability)
 
+		
+		# print(nodes[statement[0][1:]])
+		# for s in statement:
+		# 	print(s)
+		# print("\n")
 
-# def input_to_probs():
+		lines.pop(0)
+
 
 # def input_to_queries():
 
 def main():
 	lines = []
-	network = {}	
+	nodes = {}	
 
 	for line in fileinput.input():
 	    lines.append(line)
-	lines = list(map(lambda x: x.strip(), lines))
+	lines = list(map(lambda x: x.strip(''), lines))
 
 	lines = process_input(lines)
-	lines, network = input_to_nodes(lines)
-
-	for k, v in network.items():
-		print(k, v);
-
+	lines, nodes = input_to_nodes(lines)
+	lines, nodes = input_to_probs(lines, nodes)
 
 if __name__ == '__main__':
 	main()

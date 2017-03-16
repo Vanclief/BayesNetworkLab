ef834e51e7dee69b8f8ac99e469b6d2b1a7fd0e6
Franco Valencia <franco.avalencia@gmail.com>
2017-03-16 08:21:34 -0600

    Getting probability from a single node

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import fileinput
 import re
 
 # Node Class
+
+
 class Node:
     def __init__(self, parents, probabilities_table):
         self.parents = parents
@@ -11,6 +13,8 @@ class Node:
         return "Node(%s %s)" % (self.parents, self.probabilities_table)
 
 # Serialize Input into lines
+
+
 def process_input():
     lines = []
 
@@ -39,8 +43,20 @@ def parse_input(lines):
             sections[current_section].append(line)
     return sections
 
+# Remove + or - from string
+
+
+def get_nodes_from_query(query):
+    nodes = []
+    for elements in query:
+        for element in elements:
+            nodes.append(element[1:])
+
+    return nodes
 
 # Create the nodes for the Bayesian Network
+
+
 def create_nodes(sections):
     bayesian_network = {}
     nodes = sections['[Nodes]']
@@ -51,17 +67,25 @@ def create_nodes(sections):
         probabilities_table = get_probabilities_table(node, probabilities)
         bayesian_network[node] = Node(parents, probabilities_table)
 
-    # prints Bayesian Network
-    # for node in bayesian_network:
-    # 	print(node)
-    # 	print(bayesian_network[node].parents)
-    # 	for prob in bayesian_network[node].probabilities_table:
-    # 		print(prob, bayesian_network[node].probabilities_table[prob])
-    # 	print("\n")
     return bayesian_network
 
+# Prints a Bayesian Network
+
+
+def print_network(bayesian_network):
+    for node in bayesian_network:
+        print(node)
+        print(bayesian_network[node].parents)
+        for prob in bayesian_network[node].probabilities_table:
+            print(
+                prob,
+                bayesian_network[node].probabilities_table[prob]
+            )
+            print("\n")
 
 # Return a list with the name of the parents of the node
+
+
 def get_parents(node, sections):
     nodes = sections['[Nodes]']
     probabilities = sections['[Probabilities]']
@@ -103,46 +127,66 @@ def get_probabilities(node, sections):
 
 # Return a dictionary with the probabilities table of a node
 def get_probabilities_table(node, probabilities):
-	prob_table = {}
-	for line in probabilities[:]:
-		given, prob = line.split('=')
-		prob = float(prob)
-		given = given.split(",")
-		given.sort(key = lambda x: x[1:])
-		key = ""
-		if len(given) > 1:
-			for item in given:
-				key += (item)
-		true_key = "+" + node + key
-		false_key = "-" + node + key
-		prob_table[true_key] = prob
-		prob_table[false_key] = 1 - prob
-	return prob_table
+    prob_table = {}
+    for line in probabilities[:]:
+        given, prob = line.split('=')
+        prob = float(prob)
+        given = given.split(",")
+        given.sort(key=lambda x: x[1:])
+        key = ""
+        if len(given) > 1:
+            for item in given:
+                key += (item)
+        true_key = "+" + node + key
+        false_key = "-" + node + key
+        prob_table[true_key] = prob
+        prob_table[false_key] = 1 - prob
+    return prob_table
 
-# Return a list with list of queries, which are dictionaries with "queried" and "given" elements each
+# Return a list with list of queries, which are dictionaries with
+# "queried" and "given" elements each
+
+
 def create_queries(sections):
-	queries_list = []
-	queries = sections['[Queries]']
-	for line in queries:
-		query_elements = []
-		element = []
-		query = line.split('|')
-		for i in query:
-			element = i.split(',')
-			element.sort(key = lambda x: x[1:])
-			query_elements.append(element)
-		queries_list.append(query_elements)
-	for query in queries_list:
-		print(query)
-	return queries_list
+    queries_list = []
+    queries = sections['[Queries]']
+    for line in queries:
+        query_elements = []
+        element = []
+        query = line.split('|')
+        for i in query:
+            element = i.split(',')
+            element.sort(key=lambda x: x[1:])
+            query_elements.append(element)
+        queries_list.append(query_elements)
+    return queries_list
+
+
+# Process queries and get their probabilities
+def process_queries(bayesian_network, queries):
+    # print_network(bayesian_network)
+    for query in queries:
+        print(query)
+        if len(query) == 1:
+           prob = get_probability_from_node(bayesian_network, query)
+           print(prob)
+        else:
+            get_nodes_from_query(query)
+
+
+def get_probability_from_node(bayesian_network, query):
+    node_name = get_nodes_from_query(query)
+    node = bayesian_network[node_name[0]]
+    return node.probabilities_table[query[0][0]]
 
 
 def main():
 
     lines = process_input()
     sections = parse_input(lines)
-    b_n = create_nodes(sections)
+    bayesian_network = create_nodes(sections)
     queries = create_queries(sections)
+    process_queries(bayesian_network, queries)
 
 
 if __name__ == '__main__':
